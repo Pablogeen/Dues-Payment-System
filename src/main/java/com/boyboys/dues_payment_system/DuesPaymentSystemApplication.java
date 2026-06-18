610c7a755db5d3f9ffd97b77b897c5d717accea9
benandco <tettehbernard283@gmail.com>
2026-06-18 19:10:53 +0000

    Initial Commit

---
+++ src/main/java/com/boyboys/dues_payment_system/DuesPaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.boyboys.dues_payment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuesPaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuesPaymentSystemApplication.class, args);
+	}
+
+}
